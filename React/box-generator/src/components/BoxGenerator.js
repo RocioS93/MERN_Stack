8b69ff6103a6b8bd31d16964a70ab42efd01351e
RocioS93 <74567862+RocioS93@users.noreply.github.com>
2021-03-13 13:12:36 -0800

    finished Box Generator, added into form

--- React/box-generator/src/components/BoxGenerator.js
+++ React/box-generator/src/components/BoxGenerator.js
@@ -1,27 +1,83 @@
-import React, { useState } from  'react';
+import React, { useState } from "react";
 import StyledBox from "./StyledBox";
+import StyledInput from "./StyledInput";
+import StyledButton from "./StyledButton";
 
 const BoxGenerator = (props) => {
-const [color, setColor] = useState("");
+  const colorField = React.createRef();
+  const widthField = React.createRef();
 
-const createBox = e => {
-  e.preventDefault();
-  const newBox = {color};
-  console.log("bg color", newBox);
-}
+  const [state, setState] = useState({
+    boxes: [],
+    newColor: "",
+    newWidth: 0,
+  });
+
+  const onClickHandler = e => {
+    try {
+      e.preventDefault();
+      setState({
+        boxes: [...state.boxes, {
+          color: state.newColor,
+          width: state.newWidth,
+        }],
+        newColor: "",
+        newWidth: 0,
+      });
+      colorField.current.value = "";
+      widthField.current.value = "";
+    }
+    catch{
+      console.error();
+    }
+  };
+
+  const onChangeColorHandler = e => {
+    e.preventDefault();
+    setState({
+      ...state,
+      newColor: e.target.value
+    });
+  };
+
+  const onChangeWidthHandler = e => {
+    e.preventDefault();
+    setState({
+      ...state,
+      newWidth: e.target.value
+    });
+  };
+
 
 return(
   <div>
-    <form onSubmit= { createBox }>
-      <label>Color:</label>
-      <input type="text" onChange={ (e) => setColor(e.target.value) }></input>
-      <button type="submit" value="create Box">Add</button>
+    <h1>Add a new box!</h1>
+    <form>
+      <StyledInput
+        ref={colorField}
+        onChange={onChangeColorHandler}
+        placeholder="Color. Ex: red, green, blue..."
+      />
+      <StyledInput
+        ref={widthField}
+        onChange={onChangeWidthHandler}
+        placeholder="Width. Ex: 20px, 100px, 50px..."
+      />
+      <StyledButton onClick={onClickHandler}>Submit</StyledButton>
     </form>
-
-      <p>color: {color}</p>
-    <div className={ styles.box }></div>
+    <br/>
+    <div>
+      {state.boxes.map((box, index) => (
+        <StyledBox
+          key={index}
+          bgColor={box.color}
+          width={box.width}
+          height={box.width}
+        ></StyledBox>
+      ))}
+    </div>
   </div>
-);
+  );
 }
 
 export default BoxGenerator;
