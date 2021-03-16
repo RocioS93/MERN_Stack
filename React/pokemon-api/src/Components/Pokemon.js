a2a907e52d4c3c99be9a4094f1247106f299625b
RocioS93 <74567862+RocioS93@users.noreply.github.com>
2021-03-16 12:10:54 -0700

    Added fetch

---
+++ React/pokemon-api/src/Components/Pokemon.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+
+const Pokemon = (props) => {
+    const [pokemon, setPokemon] = useState([]);
+
+    const onClick = () => {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+    }
+
+    return (
+        <div>
+            <button onClick={onClick}>Fetch Pokemon</button>
+            <div>
+                {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
+                    return (<div key={index}>{pokemon.name}</div>)
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Pokemon;
